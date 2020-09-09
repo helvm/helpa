ef5a7af3cfb4ba84bb36d894414937f880b2bdc7
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-09-09 22:07:01 +0200

    refactor

--- src/main/eta/HelVM/HelPA/Assemblers/EAS/AsmParser.hs
+++ src/main/eta/HelVM/HelPA/Assemblers/EAS/AsmParser.hs
@@ -9,7 +9,6 @@ import HelVM.HelPA.Common.Value
 import Control.Applicative
 import Data.Attoparsec.Text hiding (I, D)
 import Data.Char
-import Data.Functor
 
 import qualified Data.Text as T
 
@@ -22,11 +21,11 @@ instructionListParser = skipManyComment *> skipHorizontalSpace *> many (instruct
 instructionParser :: Parser Instruction
 instructionParser =
   try zeroOperandInstructionParser
-  <|> numberOperandInstructionParser
-  <|> rParser
-  <|> dParser
-  <|> lParser
-  <|> uParser
+  <|> naturalNumberParser
+  <|> unescapedStringParser
+  <|> labelParser
+  <|> includeParser
+  <|> lineBreakParser
   <|> commentParser
 
 ----
@@ -40,29 +39,29 @@ zeroOperandInstructionParser =
   <|> zeroOperandInstruction I ["I", "Input"]
   <|> zeroOperandInstruction S ["S", "Subtract"]
   <|> zeroOperandInstruction H ["H", "Halibut"]
-    where zeroOperandInstruction i ts = (asciiCIChoices ts *> endWordParser) $> i
+    where zeroOperandInstruction i ts = i <$ (asciiCIChoices ts *> endWordParser)
 
-numberOperandInstructionParser :: Parser Instruction
-numberOperandInstructionParser = N <$> (
+naturalNumberParser :: Parser Instruction
+naturalNumberParser = N <$> (
       naturalValueParser
   <|> (asciiCI "N" *> skipHorizontalSpace *> naturalValueParser)
   <|> (asciiCI "Number" *> endWordParser *> skipHorizontalSpace *> naturalValueParser)
   )
 
-rParser :: Parser Instruction
-rParser = (skipMany1EndLine *> skipManyComment) $> R
+unescapedStringParser :: Parser Instruction
+unescapedStringParser = U <$> stringParser
 
-dParser :: Parser Instruction
-dParser = D <$> (char '*' *> fileNameParser <* char '\n')
-  
-lParser :: Parser Instruction
-lParser = L <$> (char '>' *> identifierParser <* char ':')
+labelParser :: Parser Instruction
+labelParser = L <$> (char '>' *> identifierParser <* char ':')
 
-uParser :: Parser Instruction
-uParser = U <$> stringParser
+includeParser :: Parser Instruction
+includeParser = D <$> (char '*' *> fileNameParser <* char '\n')
+
+lineBreakParser :: Parser Instruction
+lineBreakParser = R <$ (skipMany1EndLine *> skipManyComment)
 
 commentParser :: Parser Instruction
-commentParser = skipComment *> rParser
+commentParser = skipComment *> lineBreakParser
 
 skipManyComment :: Parser [()]
 skipManyComment = many (skipComment <* skipMany1EndLine)
