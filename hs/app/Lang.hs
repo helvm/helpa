1bed8b524c049a6cd2bc3cfc582c6086520a3200
Kamil Adam <kamil.adam.zabinski@gmail.com>
2022-10-05 21:59:00 +0200

    Use better Options

--- hs/app/Lang.hs
+++ hs/app/Lang.hs
@@ -2,8 +2,8 @@ module Lang where
 
 import           HelVM.HelPA.Assemblers.WSA.API.TokenType
 
---langs :: [Lang]
---langs = [HAPAPL , ASQ, EAS , WSA]
+langHelp :: String
+langHelp = "[HAPAPL , ASQ, EAS , WSA]"
 
 data Lang = HAPAPL | ASQ | EAS | WSA TokenType
   deriving stock (Eq , Read , Show)
